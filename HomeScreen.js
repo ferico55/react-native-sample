3ad014f6cc3c93ca9c5d3fa6f23ddd906ebe9bbe
Ferico Samuel <ferico55@gmail.com>
2017-11-10 20:23:37 +0700

    initial commit

---
+++ HomeScreen.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { StyleSheet, Text, View, Platform } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
+
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: "Contact List"
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+    );
+  }
+}
+
+export default HomeScreen;
